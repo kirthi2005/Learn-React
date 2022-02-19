c2930b6c4ecf29febb9bea6bb852e0357e0e4893
kirthi2005 <kiruthikard@gmail.com>
2022-02-19 08:40:53 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# return-jsx-element
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/return-jsx-element)
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  const element = <h1> I am learning React</h1>;
+  return <div>{element}</div>;
+}
